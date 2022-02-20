4d266b2550d911dc769f7c6955e3d9a31b04e18d
PixelsDE <carina.schoppe2912@gmail.com>
2022-02-20 12:27:45 +0100

    init commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,13 @@
+/*
+ * Copyright Notice for KotlinServerClientMinecraft
+ * Copyright (c) at Carina Sophie Schoppe 2022
+ * File created on 20.02.22, 12:27 by Carina The Latest changes made by Carina on 20.02.22, 12:22 All contents of "build.gradle.kts" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
+ * at Carina Sophie Schoppe. All rights reserved
+ * Any type of duplication, distribution, rental, sale, award,
+ * Public accessibility or other use
+ * requires the express written consent of Carina Sophie Schoppe.
+ */
+
 import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
--- src/main/kotlin/me/carina-sophie/KotlinServerMC.kt
+++ src/main/kotlin/me/carina-sophie/KotlinServerMC.kt
@@ -1,3 +1,13 @@
+/*
+ * Copyright Notice for KotlinServerClientMinecraft
+ * Copyright (c) at Carina Sophie Schoppe 2022
+ * File created on 20.02.22, 12:27 by Carina The Latest changes made by Carina on 20.02.22, 12:24 All contents of "KotlinServerMC.kt" are protected by copyright. The copyright law, unless expressly indicated otherwise, is
+ * at Carina Sophie Schoppe. All rights reserved
+ * Any type of duplication, distribution, rental, sale, award,
+ * Public accessibility or other use
+ * requires the express written consent of Carina Sophie Schoppe.
+ */
+
 package me.`carina-sophie`
 
 import org.bukkit.ChatColor
